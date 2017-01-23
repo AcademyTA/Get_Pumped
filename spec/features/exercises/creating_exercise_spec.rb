0656d4922c976d9ff6ae3eb35f1733f3c1a9bb8f
Jason <cameljeet@gmail.com>
2017-01-23 01:21:28 -0800

    BDD validations for exercise model

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -20,11 +20,29 @@ RSpec.feature "Creating exercise" do
 
     fill_in "Duration", with: 70
     fill_in "Workout details",  with: "Weight lifting"
-    fill_in "Activity date",  with: "2017-01-26"
+    fill_in "Activity date",  with: "2016-07-26"
     click_button "Create Exercise"
 
     expect(page).to have_content("Exercise has been created")
     exercise = Exercise.last
     expect(current_path).to eq(user_exercise_path(@jason, exercise))
+  end
+
+  scenario "with invalid inputs" do
+    visit "/"
+
+    click_link "My Dashboard"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+
+    fill_in "Duration", with: ""
+    fill_in "Workout details",  with: ""
+    fill_in "Activity date",  with: ""
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
   end
 end
